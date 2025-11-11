0c36c93bfb3da73450be31314c96c3469a40d621
bikathi <bikathimartin@outlook.com>
2025-11-11 06:16:20 +0300

    Add prettier configs, eslint configs and basic structure of app alongside Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  const hbsInstance = hbs.create({
+    extname: 'hbs',
+    defaultLayout: 'layout_main',
+    layoutsDir: join(__dirname, '..', 'views', 'layouts'),
+    partialsDir: join(__dirname, '..', 'views', 'partials'),
+    helpers: {
+      // Example helper: toUpperCase
+      toUpperCase: (str: string) => str.toUpperCase(),
+      // Example helper: conditional check
+      isEqual: (a: any, b: any) => a === b,
+    },
+  });
+  app.engine('hbs', hbsInstance.engine);
+  app.setViewEngine('hbs');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
